bd43e9e01de432770c255653976b69891852290c
mac-wangsh <46606405@qq.com>
2023-04-12 21:35:33 +0800

    feat: login page

--- projects/web/src/App.vue
+++ projects/web/src/App.vue
@@ -2,7 +2,8 @@
 import { onMounted, ref } from 'vue'
 import type { LoginForm } from '@yy-admin/common-core'
 import { code, login } from '@yy-admin/common-core'
-import { message } from 'ant-design-vue'
+import { Form, message } from 'ant-design-vue'
+import type { Rule } from 'ant-design-vue/es/form'
 import { encrypt } from '@yy-admin/common-utils'
 
 const loginForm = ref<LoginForm>({
@@ -10,6 +11,17 @@ const loginForm = ref<LoginForm>({
   password: '',
   code: '',
   uuid: '',
+})
+const loginRules = ref<{ [P in keyof Partial<LoginForm>]: Rule[] }>({
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'change' },
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'change' },
+  ],
+  code: [
+    { required: true, message: '请输入验证码', trigger: 'change' },
+  ],
 })
 const codeImg = ref('')
 
@@ -22,19 +34,28 @@ function handleRefreshImg() {
 
 onMounted(handleRefreshImg)
 
+const { validate, validateInfos } = Form.useForm(loginForm, loginRules)
 function handleLoadingAction() {
-  login({ ...loginForm.value, password: encrypt(loginForm.value.password) as string }).then(() => {
-    message.success('登录成功')
+  validate().then(() => {
+    login({ ...loginForm.value, password: encrypt(loginForm.value.password) as string }).then(() => {
+      message.success('登录成功')
+    })
   })
 }
 </script>
 
 <template>
-  <a-form w-300px>
-    <a-input v-model:value="loginForm.username" placeholder="请输入用户名" />
-    <a-input v-model:value="loginForm.password" placeholder="请输入密码" />
-    <a-input v-model:value="loginForm.code" placeholder="请输入验证码" />
-    <img w-300px :src="codeImg" @click="handleRefreshImg">
+  <a-form :model="loginForm" w-300px>
+    <a-form-item v-bind="validateInfos.username">
+      <a-input v-model:value="loginForm.username" placeholder="请输入用户名" />
+    </a-form-item>
+    <a-form-item v-bind="validateInfos.password">
+      <a-input v-model:value="loginForm.password" placeholder="请输入密码" />
+    </a-form-item>
+    <a-form-item v-bind="validateInfos.code">
+      <a-input v-model:value="loginForm.code" placeholder="请输入验证码" />
+      <img w-300px :src="codeImg" @click="handleRefreshImg">
+    </a-form-item>
     <a-button type="primary" @click="handleLoadingAction">
       登录
     </a-button>
