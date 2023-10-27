9ad6bc776cf1d5614bd93ab91cb4fbd8f6c3f16b
a949066041 <949066041@qq.com>
2023-10-27 21:12:53 +0800

    chore: update dep

---
+++ config/md-vue/src/genApi.ts
@@ -0,0 +1,11 @@
+// const fileRegex = /\.(my-file-ext)$/
+
+// export default function myPlugin() {
+//   return {
+//     name: 'transform-file',
+
+//     transform(src, id) {
+//       console.log()
+//     },
+//   }
+// }
