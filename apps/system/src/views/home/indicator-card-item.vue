068f892274c2546b6649315a3c6e6c0df887dbce
a949066041 <949066041@qq.com>
2024-01-16 21:16:14 +0800

    fix: 代码整理

---
+++ apps/system/src/views/home/indicator-card-item.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+defineOptions({
+  name: 'IndicatorCardItem',
+})
+
+withDefaults(defineProps<{ title: string, value: number, suffix?: string, prefix?: string }>(), { suffix: '', prefix: '' })
+</script>
+
+<template>
+  <div class=" card-item__box">
+    <div class=" flex justify-between">
+      <span class=" op65">{{ title }}</span>
+      <i-ph:question-light />
+    </div>
+    <div class=" text-3xl">
+      {{ prefix }}<n-number-animation :from="0" :to="value" show-separator />{{ suffix }}
+    </div>
+    <slot />
+    <CardReuseTemplate class="min-h-11.5" />
+    <n-divider class="!my-2" />
+    <slot name="footer" />
+  </div>
+</template>
+
+<style lang="less" scoped>
+.card-item__box {
+  @apply px-24px pt-20px pb-8px rounded-xl
+    shadow-[0_1px_2px_0_rgba(0,0,0,0.03),0_1px_6px_-1px_rgba(0,0,0,0.02),0_2px_4px_0_rgba(0,0,0,0.02)]
+    border
+    border-solid
+    dark:border-color-[rgba(255,255,255,0.09)]
+    border-color-[rgb(239,239,245)];
+}
+</style>
--- apps/system/src/views/home/indicator-card.vue
+++ apps/system/src/views/home/indicator-card.vue
@@ -4,11 +4,15 @@ import { EchartsView, smallLineOrBarOption } from '@yy-admin/components-chart'
 import { UserApi } from '@yy-admin/common-apis'
 import { LazyBox, useLazyBox } from '@yy-admin/components-base'
 import dayjs from 'dayjs'
+import { createReusableTemplate } from '@vueuse/core'
 import IndicatorText from '../../components/indicator-text.vue'
+import IndicatorCardItem from './indicator-card-item.vue'
 
 defineOptions({
   name: 'IndicatorCard',
 })
+
+const [CardDefineTemplate, CardReuseTemplate] = createReusableTemplate()
 
 function genData() {
   return Array.from({ length: 30 }, (_, i) => ({ name: dayjs().subtract(i, 'day').format('YYYY-MM-DD'), value: Math.random() * 100 })).reverse()
@@ -37,78 +41,54 @@ const { config, handleRenderCb } = useLazyBox({
 </script>
 
 <template>
+  <CardDefineTemplate>
+    <div class=" flex items-end op88">
+      <div class=" flex flex-wrap">
+        <IndicatorText class=" mr-4" label="周同比" value="12%">
+          <template #suffix-icon>
+            <i-teenyicons:up-solid class=" w-3 text-green-700 ml-1" />
+          </template>
+        </IndicatorText>
+        <IndicatorText label="日同比" value="11%">
+          <template #suffix-icon>
+            <i-teenyicons:up-solid class=" w-3 text-red-700 ml-1 rotate-180" />
+          </template>
+        </IndicatorText>
+      </div>
+    </div>
+  </CardDefineTemplate>
   <div class=" home-card">
     <n-grid :x-gap="12" :y-gap="12" responsive="screen" cols="1 s:1 m:2 xl:4">
       <n-gi>
-        <div class=" home-card__box">
-          <div class=" flex justify-between">
-            <span class=" op65">总销售额</span>
-            <i-ph:question-light />
-          </div>
-          <div class=" text-3xl">
-            ¥<n-number-animation :from="0" :to="12039" show-separator />
-          </div>
-          <div class=" min-h-11.5 flex items-end op88">
-            <div class=" flex flex-wrap">
-              <IndicatorText class=" mr-4" label="周同比" value="12%">
-                <template #suffix-icon>
-                  <i-teenyicons:up-solid class=" w-3 text-green-700 ml-1" />
-                </template>
-              </IndicatorText>
-              <IndicatorText label="日同比" value="11%">
-                <template #suffix-icon>
-                  <i-teenyicons:up-solid class=" w-3 text-red-700 ml-1 rotate-180" />
-                </template>
-              </IndicatorText>
-            </div>
-          </div>
-          <n-divider class="!my-2" />
-          <IndicatorText label="日销售额" value="￥12,423" />
-        </div>
+        <IndicatorCardItem title="总销售额" :value="12039" prefix="¥">
+          <CardReuseTemplate class="min-h-11.5" />
+          <template #footer>
+            <IndicatorText label="日销售额" value="￥12,423" />
+          </template>
+        </IndicatorCardItem>
       </n-gi>
       <n-gi>
-        <div class=" home-card__box">
-          <div class=" flex justify-between">
-            <span class="  op65">访问量
-            </span>
-            <i-ph:question-light />
-          </div>
-          <div class=" text-3xl">
-            <n-number-animation :from="0" :to="12039" show-separator />
-          </div>
+        <IndicatorCardItem title="访问量" :value="2323" prefix="¥">
           <LazyBox :height="46" title="测试" v-bind="config.test2" @render="handleRenderCb">
             <EchartsView ref="chart2" class=" h-200px w-full" />
           </LazyBox>
-          <n-divider class="!my-2" />
-          <IndicatorText label="日访问量" value="1,234" />
-        </div>
+          <template #footer>
+            <IndicatorText label="日访问量" value="1,234" />
+          </template>
+        </IndicatorCardItem>
       </n-gi>
       <n-gi>
-        <div class=" home-card__box">
-          <div class=" flex justify-between">
-            <span class="  op65">支付笔数
-            </span>
-            <i-ph:question-light />
-          </div>
-          <div class=" text-3xl">
-            <n-number-animation :from="0" :to="3344" show-separator />
-          </div>
+        <IndicatorCardItem title="支付笔数" :value="3344">
           <LazyBox :height="46" title="测试" v-bind="config.test3" @render="handleRenderCb">
             <EchartsView ref="chart3" class=" h-200px w-full" />
           </LazyBox>
-          <n-divider class="!my-2" />
-          <IndicatorText label="转化率" value="60%" />
-        </div>
+          <template #footer>
+            <IndicatorText label="转化率" value="60%" />
+          </template>
+        </IndicatorCardItem>
       </n-gi>
       <n-gi>
-        <div class=" home-card__box">
-          <div class=" flex justify-between">
-            <span class="  op65">运营活动效果</span>
-            <i-ph:question-light />
-          </div>
-          <div class=" text-3xl">
-            <n-number-animation :from="0" :to="70" show-separator />%
-          </div>
+        <IndicatorCardItem title="运营活动效果" :value="70" suffix="%">
           <div class=" min-h-11.5 flex items-center op88">
             <n-progress
               type="line"
@@ -117,36 +97,11 @@ const { config, handleRenderCb } = useLazyBox({
               processing
             />
           </div>
-          <n-divider class="!my-2" />
-          <div class=" flex items-end op88">
-            <div class=" flex flex-wrap">
-              <IndicatorText class=" mr-4" label="周同比" value="12%">
-                <template #suffix-icon>
-                  <i-teenyicons:up-solid class=" w-3 text-green-700 ml-1" />
-                </template>
-              </IndicatorText>
-              <IndicatorText label="日同比" value="11%">
-                <template #suffix-icon>
-                  <i-teenyicons:up-solid class=" w-3 text-red-700 ml-1 rotate-180" />
-                </template>
-              </IndicatorText>
-            </div>
-          </div>
-        </div>
+          <template #footer>
+            <CardReuseTemplate />
+          </template>
+        </IndicatorCardItem>
       </n-gi>
     </n-grid>
   </div>
 </template>
-
-<style lang="less" scoped>
-.home-card {
-  &__box {
-    @apply px-24px pt-20px pb-8px rounded-xl
-      shadow-[0_1px_2px_0_rgba(0,0,0,0.03),0_1px_6px_-1px_rgba(0,0,0,0.02),0_2px_4px_0_rgba(0,0,0,0.02)]
-      border
-      border-solid
-      dark:border-color-[rgba(255,255,255,0.09)]
-      border-color-[rgb(239,239,245)];
-  }
-}
-</style>
