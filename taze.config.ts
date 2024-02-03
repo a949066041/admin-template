e31f0b9edfc8f7c0fe9a6b23707143140e37115c
a949066041 <949066041@qq.com>
2024-02-03 15:58:03 +0800

    chore: update dep

--- taze.config.ts
+++ taze.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from 'taze'
 
 export default defineConfig({
-  // ignore packages from bumping
-  exclude: [
-    'webpack',
-  ],
-  // fetch latest package info from registry without cache
   force: true,
   // write to package.json
   write: true,
