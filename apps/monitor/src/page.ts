3e95861bec3f1801b3963c146626515a9a55ea36
a949066041 <949066041@qq.com>
2024-07-22 16:15:35 +0800

    chore: update import

--- apps/monitor/src/page.ts
+++ apps/monitor/src/page.ts
@@ -5,10 +5,11 @@ export function monitorPageList() {
   const context = import.meta.webpackContext('./views', {
     recursive: true,
     regExp: /\.vue$/,
-    mode: 'lazy',
   })
   return addModulePageList(context.keys().reduce((base, item) => {
-    base[item.replace('./', '')] = context(item)
+    // eslint-disable-next-line ts/ban-ts-comment
+    // @ts-expect-error
+    base[item.replace('./', '')] = context(item).default
     return base
   }, {} as AsyncRouters), 'monitor')
 }
