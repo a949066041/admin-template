1cc825af6b969a73bf6b1bf4a62accb43a03e8c4
a949066041 <949066041@qq.com>
2024-07-29 12:14:28 +0800

    chore: update dep

--- components/admin/src/router.ts
+++ components/admin/src/router.ts
@@ -15,7 +15,7 @@ export function createYyRouter(home?: RouteRecordRaw) {
     baseRoutres.push(home as any)
 
   return createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory('/'),
     routes: baseRoutres,
   })
 }
